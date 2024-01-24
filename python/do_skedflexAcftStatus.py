5337814b168a705c164bbc223549afec35ae6152
Nate Daiger <nate@daiger.com>
2024-01-24 11:04:06 -0800

    do actaully correct json serializing
    Intentionally rewriting as little as possible; obviously a better way
    to do this whole thing is to build the final dict as you go instead of
    a bunch of disparate arrays that you later combine.

--- python/do_skedflexAcftStatus.py
+++ python/do_skedflexAcftStatus.py
@@ -8,6 +8,7 @@ Usage:
 
 import sys
 import re
+import json
 import mysql.connector
 from mysql.connector import Error
 from datetime import date, timedelta, datetime
@@ -16,17 +17,18 @@ from datetime import date, timedelta, datetime
 
 
 def run_query(start_date, end_date):
-    
-    
+
+
     tailNum = []
     acftType = []
-    
+
     statusTailNum = []
     statusCurrent = []
     statusType = []
     statusDate = []
     statusNote = []
-    
+
+
     try:
         connection = mysql.connector.connect(host='127.0.0.1',
                                             port=3306,
@@ -36,88 +38,98 @@ def run_query(start_date, end_date):
                                             auth_plugin='mysql_native_password',
                                             use_pure=True)
         cursor = connection.cursor(dictionary=True)
-        
-        
+
+
         sql = """SELECT aircraft, type FROM Aircraft WHERE active = 'yes' """
 
 
         cursor.execute(sql)
         records = cursor.fetchall()
-        
+
         for row in records:
-            
+
             # print(row)
             tailNum.append(row['aircraft'])
             acftType.append(row['type'])
-            
+
         # print(tailNum)
         # print(acftType)
         # print('status ',statusCurrent)
-        
-        
+
+
         index = 0
         while index < len(tailNum):
-            
+
             searchTail = tailNum[index]
             statusTailNum.append(searchTail)
             statusCurrent.append('Up')
             statusType.append(acftType[index])
             statusDate.append(start_date)
             statusNote.append('')
-            
+
             sql = """SELECT * FROM SkedFlexTailStatus WHERE startTimes <= """
-            
+
             sql += "'"+start_date+"T11:01:00'"
             sql += """ AND endTimes >= """
             sql += "'"+end_date+"T07:59:00'"
             sql += """ AND names = 'Maintenance' AND tails = """
             sql += "'"+searchTail+"'"
-            
+
             # print('sql ',sql)
-            
-            
+
+
             cursor.execute(sql)
             records = cursor.fetchall()
-            
+
             # print(start_date)
             # print(searchTail)
             # print('records ',len(records))
             # print(len(records))
-            
+
             for row in records:
-            
-          
+
+
                 statusCurrent[index] = 'Down'
                 statusNote[index] = row['notes']
-                
-            
+
+
             index += 1
-            
+
         # print(statusTailNum)
         # print(statusCurrent)
         # print(statusType)
         # print('notes ',statusNote)
-        
-        jsonList = []
-        jsonLine = ''
-        
+
+        # jsonList = []
+        # jsonLine = ''
+
         index = 0
-        
+
+        statusByTailNum = {}
+
+
         while index < len(statusTailNum):
-            jsonLine='{"Aircraft":"'+statusTailNum[index]+'","Type":"'+statusType[index]+'","Status":"'+statusCurrent[index]+'","Note":"'+statusNote[index]+'","Date":"'+statusDate[index]+'"}'
-              
-                
-            jsonList.append(jsonLine)
-            
+            # jsonLine='{"Aircraft":"'+statusTailNum[index]+'","Type":"'+statusType[index]+'","Status":"'+statusCurrent[index]+'","Note":"'+statusNote[index]+'","Date":"'+statusDate[index]+'"}'
+
+            statusByTailNum['Aircraft'] = statusTailNum[index]
+            statusByTailNum['Type'] = statusType[index]
+            statusByTailNum['Status'] = statusCurrent[index]
+            statusByTailNum['Note'] = statusNote[index]
+            statusByTailNum['Date'] = statusDate[index]
+
+
+            # jsonList.append(jsonLine)
+
             index += 1
 
-        jsonOutput=str(jsonList)
-        
-        jsonOutput=jsonOutput.translate({ord("'"):None})
-        jsonOutput=jsonOutput.replace("}, {","},{")    
-        
-        print(jsonOutput)
-    
+        # jsonOutput=str(jsonList)
+
+        # jsonOutput=jsonOutput.translate({ord("'"):None})
+        # jsonOutput=jsonOutput.replace("}, {","},{")
+
+        #print(jsonOutput)
+        print(json.dumps(statusByTailNum))
+
 
     except mysql.connector.Error as error:
         print("Failed to select from MySQL table {}".format(error))
@@ -127,23 +139,23 @@ def run_query(start_date, end_date):
         if connection.is_connected():
             cursor.close()
             connection.close()
-        
-        
+
+
 
 
 if __name__ == '__main__':
-    
-    try:   
+
+    try:
         start_date = sys.argv[1]
     except:
         start_date = date.today()
-     
-    try:   
+
+    try:
         end_date = sys.argv[2]
     except:
-        end_date = date.today() 
-     
+        end_date = date.today()
+
     # print('start_date ',start_date)
     # print('end_date ',end_date)
 
-    run_query(start_date,end_date) 
+    run_query(start_date,end_date)
